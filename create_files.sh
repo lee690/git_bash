f59ba7ed9f72f42c830f1dc8974c69e858b1d552
Christopher Lee <chrisleejean@gmail.com>
2023-11-23 23:34:00 -0500

    Update create_files.sh
    Added script descriptions

--- create_files.sh
+++ create_files.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Bash script requirements:
+# Create 25 empty (0 KB) files. 
+# The file names should be <yourName><number>, <yourName><number+1>, <yourName><number+2>, and so on.
+# Design the script so that each time it is run, it creates the next batch of 25 files with increasing numbers starting with the last or maximum number that already exists.
+# Do not hard code these numbers. You need to generate them by using automation.
+# Test the script. Display a long list of the directory and its contents to validate that the script created the expected files.
+
 yourName="yourName"
 directory="files_directory"
 
